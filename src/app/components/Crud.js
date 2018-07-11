e0db36e65de58b6a56cd26faa4b4f3d4887b2d2d
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-07-11 10:47:33 +0200

    Start enviroment

--- src/app/components/Crud.js
+++ src/app/components/Crud.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
-import NavBar from './NavBar';
+import { MuiThemeProvider } from '@material-ui/core';
+
+import { theme } from './common/muiTheme';
+import Header from './common/Header';
 
 export default class Crud extends Component {
     render() {
         return (
-            <NavBar />
+            <MuiThemeProvider theme={theme}>
+                <Header />
+            </MuiThemeProvider>
         );
     }
 }
---
+++ src/app/components/UserList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import UserItem from './UserItem';
+
+class UserList extends Component {
+    componentDidMount() {
+        this.userList = localStorage.getItem('UserList') || [];
+    }
+
+    render() {
+        const createUsers = this.userList.map((user) =>
+            (
+                <div
+                    key={`_${user.id}`}
+                >
+                    <UserItem user={user} />
+                </div>
+            ),
+        );
+
+        return (
+            <div>
+                {createUsers}
+            </div>
+        );
+    }
+}
+
+export default UserList;
