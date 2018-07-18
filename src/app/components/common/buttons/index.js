766120049c41a8d0219a13221954eb2d830f172c
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-07-18 16:30:53 +0200

    Button

--- src/app/components/common/buttons/index.js
+++ src/app/components/common/buttons/index.js
@@ -1,45 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { Link } from 'react-router-dom';
 
 import PropTypes from 'prop-types';
 
-import { withStyles } from '@material-ui/core/styles';
-
 import Button from '@material-ui/core/Button';
 
-const styles = theme => ({
-    button: {
-        margin: theme.spacing.unit,
-    },
-    input: {
-        display: 'none',
-    },
-});
+const ContainedButtons = (props) => {
+    const {
+        content,
+        color,
+        variant,
+        onClick,
+        route,
+    } = props;
 
-class ContainedButtons extends Component {
-    render() {
-        const {
-            content,
-            color,
-            variant,
-            onClick,
-            route,
-        } = this.props;
-
-        return (
-            <Button
-                variant={variant}
-                color={color}
-                component={Link}
-                to={route}
-                {...{ onClick }}
-            >
-                {content}
-            </Button>
-        );
-    }
-}
+    return (
+        <Button
+            variant={variant}
+            color={color}
+            component={Link}
+            to={route}
+            {...{ onClick }}
+        >
+            {content}
+        </Button>
+    );
+};
 
 ContainedButtons.propTypes = {
     content: PropTypes.element.isRequired,
@@ -49,4 +36,4 @@ ContainedButtons.propTypes = {
     onClick: PropTypes.func,
 };
 
-export default withStyles(styles)(ContainedButtons);
+export default ContainedButtons;
